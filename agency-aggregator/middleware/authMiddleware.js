d6f1c66ede98f24384a0d1c8f1c96a02076bbf47
Taofiq Abdulsalam <156853865+WBLT4U@users.noreply.github.com>
2024-10-04 18:13:08 +0100

    Update authMiddleware.js

--- agency-aggregator/middleware/authMiddleware.js
+++ agency-aggregator/middleware/authMiddleware.js
@@ -8,6 +8,8 @@ const protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log('Decoded Token:', decoded); // Log the decoded token
+
       req.user = await User.findById(decoded.id).select('-password');
 
       if (!req.user) {
